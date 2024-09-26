c7a245230ffd16696d5414656aae11df5ec5faa2
RandyTsai0103 <waterblue4019@hotmail.com>
2024-09-26 14:33:58 +0800

    12

--- main.py
+++ main.py
@@ -23,5 +23,6 @@ if __name__ == '__main__':
     print_hi('Randy9')
     print_hi('Randy10')
     print_hi('Randy11')
+    print_hi('Randy12')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
